1ada387bd02fe59a1721b0191e697ce1e2eaed38
darkMarcus <askardatkambek@gmail.com>
2021-02-04 17:32:05 +0600

    second commit

--- src/bitlab/askar/module2/lesson8/DBManager.java
+++ src/bitlab/askar/module2/lesson8/DBManager.java
@@ -10,52 +10,52 @@ public class DBManager {
 
     private Connection connection;
 
-    public void connect(){
-        try{
+    public void connect() {
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/bitlab_bd6?useUnicode=true&serverTimezone=UTC","root", ""
+                    "jdbc:mysql://localhost:3306/bitlab_bd6?useUnicode=true&serverTimezone=UTC", "root", ""
             );
             System.out.println("CONNECTED");
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
 
-    public void addDog(Dog dog){
-        try{
+    public void addDog(Dog dog) {
+        try {
             PreparedStatement st = connection.prepareStatement("INSERT INTO dogs(name, age, weight) values(?,?,?)");
 
-            st.setString(1,dog.getName());
-            st.setInt(2,dog.getAge());
-            st.setDouble(3,dog.getWeight());
+            st.setString(1, dog.getName());
+            st.setInt(2, dog.getAge());
+            st.setDouble(3, dog.getWeight());
             st.executeUpdate();
             st.close();
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
 
-    public ArrayList<Dog> getAllDogs(){
+    public ArrayList<Dog> getAllDogs() {
         ArrayList<Dog> dogs = new ArrayList<>();
-        try{
+        try {
             PreparedStatement st = connection.prepareStatement("SELECT * FROM dogs");
             ResultSet rs = st.executeQuery();
 
-            while (rs.next()){
+            while (rs.next()) {
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
                 int age = rs.getInt("age");
                 double weight = rs.getDouble("weight");
 
-                dogs.add(new Dog(id,name,age,weight));
+                dogs.add(new Dog(id, name, age, weight));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -63,8 +63,8 @@ public class DBManager {
     }
 
 
-    public void updateDog(Dog dog){
-        try{
+    public void updateDog(Dog dog) {
+        try {
             PreparedStatement st = connection.prepareStatement("UPDATE dogs set name = ?, age = ?, weight = ? where id = ?");
 
             st.setString(1, dog.getName());
@@ -75,25 +75,24 @@ public class DBManager {
             st.executeUpdate();
 
             st.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
 
-    public void deleteDog(int id){
-        try{
+    public void deleteDog(int id) {
+        try {
             PreparedStatement st = connection.prepareStatement("DELETE FROM dogs where id = ?");
 
             st.setLong(1, id);
             st.executeUpdate();
 
             st.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 
 
 }
